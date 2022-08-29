8cf896590e879c07afe6a465a8cef624f31ebb29
git config --global user.name AlexanderMacLennanBearing <alexander.maclennan@bearingpoint.com>
2022-08-29 14:16:30 +0200

    add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
