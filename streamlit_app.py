ee12f022a0fc7d4902ef30a3eacea7c4dc0462da
git config --global user.name AlexanderMacLennanBearing <alexander.maclennan@bearingpoint.com>
2022-10-04 18:43:05 +0200

    add box

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,13 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice_2 = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('Thanks for adding', fruit_choice_2)
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+        return 'Thanks for adding' + new_fruit
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
